4a965291a40509e2c9775a404959a28900415c2a
Johnny Gonzales <dimensions122@gmail.com>
2020-05-25 15:53:48 -0400

    list is showing

--- src/components/movie_right.js
+++ src/components/movie_right.js
@@ -5,11 +5,23 @@ class MoviesRight extends Component {
     render() {
         return (
             <div>
-                {MovieList.map((movie, index) => {
-                    if (index >= 125) {
-                        return <li>{movie.title}</li>
-                    }
-                })}
+                {MovieList.map((movie, index) => index >= 125 ? <li 
+                key={index} className="details">
+                    <div className="details__title">
+                        <h2 id="movieNumber">{index+1 + '. '}</h2> 
+                        <a href={movie["movie-url"]}>
+                            <h2>{movie.title}</h2>
+                        </a>
+                        <h4 id="duration">
+                            {movie.duration}
+                        </h4>
+                    </div>
+                    <div className="details__gyr">
+                        <h4>genre: {movie.genre}</h4>
+                        <h4>year: {movie.year}</h4>
+                        <h4>rating:{movie.rating}</h4>
+                    </div>
+                </li> : null)}
             </div>
         )
     }
